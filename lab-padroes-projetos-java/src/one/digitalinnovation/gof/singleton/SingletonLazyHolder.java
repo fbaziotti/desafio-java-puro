ddb3faa5d3f71969991a96ed7a39752d2f147926
fbaziotti <fernandobaziotti@gmail.com>
2024-11-14 14:42:14 -0300

    Desafio de Projeto com Java Puro

---
+++ lab-padroes-projetos-java/src/one/digitalinnovation/gof/singleton/SingletonLazyHolder.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "Lazy Holser".
+ * @see <a href="https://stackoverflow.com/a/24018148">Referencia</a>
+ * @author fbaziotti
+ */
+
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder {
+        private static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    private SingletonLazyHolder() {
+        super();
+    }
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+}
